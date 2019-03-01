5a0b8a9ce7d8772e94e61312f06afdff8b1652f2
zhangyongwang <20225614@qq.com>
2019-03-01 18:49:50 +0800

    改回代码

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -18,24 +18,22 @@ public class GreetingController {
 
     //  当用户访问 /greeting 这个路径的时候 ， 请Spring框架调用这个方法去执行相关的逻辑
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         // count += 1 ; return counter
-        // return new Greeting(counter.incrementAndGet(), String.format(template, name));
-
-        return String.format(template, name);
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
 
 
     }
 
-    @RequestMapping("/spider")
-    public String spider(@RequestParam(value = "startUrl", defaultValue = "") String startUrl) {
-        Spider spider = new Spider(startUrl);
-
-        spider.start();
-
-        String result = spider.getResult();
-        return result;
-    }
+//    @RequestMapping("/spider")
+//    public String spider(@RequestParam(value = "startUrl", defaultValue = "") String startUrl) {
+//        Spider spider = new Spider(startUrl);
+//
+//        spider.start();
+//
+//        String result = spider.getResult();
+//        return result;
+//    }
 
     // 框架  -- 框架负责调用你的代码
     // 库    --  我们去调用库的代码
