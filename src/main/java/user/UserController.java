dd20e17b8bba141ad5cea2d89805d85666f05ee4
zhangyongwang <20225614@qq.com>
2019-03-02 14:09:11 +0800

    Spring 响应自定义http状态

--- src/main/java/user/UserController.java
+++ src/main/java/user/UserController.java
@@ -20,26 +20,41 @@ public class UserController {
     }
 
     @GetMapping("/users/{name}")
-    User getUser(@PathVariable String name) {
-        return users.get(name);
+    ResponseEntity<User> getUser(@PathVariable String name) {
+        if (users.containsKey(name)) {
+            return new ResponseEntity<>(users.get(name), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/users")
+        // 自定义返回的状态码
     ResponseEntity<User> newUser(@RequestBody User user) {
         users.put(user.getName(), user);
+        // 创建成功后返回 User对象 ，以及自定义的状态值 201
         return new ResponseEntity<>(users.get(user.getName()), HttpStatus.CREATED);
     }
 
     @PutMapping("/users/{name}")
-    User updateUser(@PathVariable String name, @RequestBody User updatedUser) {
-        User user = users.get(name);
-        user.setContent(updatedUser.getContent());
-        return user;
+    ResponseEntity<User> updateUser(@PathVariable String name, @RequestBody User updatedUser) {
+        if (users.containsKey(name)) {
+            User user = users.get(name);
+            user.setContent(updatedUser.getContent());
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/users/{name}")
-    void deleteUser(@PathVariable String name) {
-        users.remove(name);
+    ResponseEntity<Void> deleteUser(@PathVariable String name) {
+        if (users.containsKey(name)) {
+            users.remove(name);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 
